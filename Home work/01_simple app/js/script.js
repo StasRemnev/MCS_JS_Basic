3ea925df6bac8195b3260e95d5e572fe75070fca
Stas Remnev <remnev.stas@gmail.com>
2021-03-05 14:14:01 +0300

    Update script.js
    fix defect

--- Home work/01_simple app/js/script.js
+++ Home work/01_simple app/js/script.js
@@ -1,10 +1,10 @@
-let totalCash = Number (prompt('Сколько у тебя кэша в барсетке?'));
+let totalCash = Number(prompt('Сколько у тебя кэша в барсетке?'));
 
-let watchesCount = Number (prompt ('Скольо часов хочешь купить?'));
-let earringsCount = Number (prompt ('Сколько сережек с брюликами купишь?'));
+let watchesCount = Number(prompt ('Скольо часов хочешь купить?'));
+let earringsCount = Number(prompt ('Сколько сережек с брюликами купишь?'));
 
-let watchesSum = Number (prompt('Цена одних часов?')) * watchesCount;
-let earringsSum = Number (prompt('Цена одних сережек?')) * earringsCount;
+let watchesSum = Number(prompt('Цена одних часов?')) * watchesCount;
+let earringsSum = Number(prompt('Цена одних сережек?')) * earringsCount;
 
 
 let totalBuy = document.body.innerHTML = totalCash >= watchesSum + earringsSum;
